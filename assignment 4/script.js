d18bf552cd49d020aa59b79fb7e0cac0ed92194b
ksudhagayathri <129074885+ksudhagayathri@users.noreply.github.com>
2023-04-22 16:30:44 +0530

    update

---
+++ assignment 4/script.js
@@ -0,0 +1,19 @@
+var names=new Array();
+names[0]="hari";
+names[1]="john";
+names[2]="jen";
+names[3]="jeason";
+names[4]="sonu";
+names[5]="ram";
+names[6]="sitha";
+names[7]="joy";
+names[8]="sai";
+names[9]="jim";
+for(var i=0;i < names.length;i++){
+if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+    console.log("Goodbye" +names[i])
+}
+else{
+console.log("hello" +names[i])
+} 
+}
